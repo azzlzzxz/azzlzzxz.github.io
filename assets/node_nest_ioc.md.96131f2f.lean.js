5e25071cc457973f959e533146b95ad0c2ef7f51
azzlzzxz <37786423+azzlzzxz@users.noreply.github.com>
2023-10-09 19:21:17 +0800

    Deploying to gh-pages from  @ d41d0cdb09484217ad8c9c11c1b5c4e44c1ad1d5 🚀

---
+++ assets/node_nest_ioc.md.96131f2f.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o}from"./chunks/framework.a5035e6c.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"node/nest/ioc.md","lastUpdated":1696850414000}'),a={name:"node/nest/ioc.md"};function n(s,c,r,d,i,p){return t(),o("div")}const f=e(a,[["render",n]]);export{m as __pageData,f as default};
